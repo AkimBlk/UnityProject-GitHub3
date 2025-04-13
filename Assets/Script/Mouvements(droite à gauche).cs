c784c393d7020c8296f752df998b634caf28fa32
Mohamed El Jari <mohamed.eljari@student.helb-prigogine.be>
2025-04-13 23:34:32 +0200

    Cibles du niveau 2 + destruction etc..

---
+++ Assets/Script/Mouvements(droite à gauche).cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Moving2Target : MonoBehaviour
+{
+    public float speed = 2f;       // Vitesse du mouvement
+    public float distance = 3f;    // Distance maximale à parcourir
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float offset = Mathf.PingPong(Time.time * speed, distance);
+        Vector3 newPos = startPosition - Vector3.forward * offset; // ← Remarquez le **moins**
+        transform.position = newPos;
+    }
+}
---
+++ Assets/Script/MovingTarget.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MovingTarget : MonoBehaviour
+{
+    public float speed = 2f;       // Vitesse du mouvement
+    public float distance = 3f;    // Distance maximale à parcourir
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float offset = Mathf.PingPong(Time.time * speed, distance);
+        Vector3 newPos = startPosition + Vector3.forward * offset; // ← Z au lieu de X
+        transform.position = newPos;
+    }
+}
